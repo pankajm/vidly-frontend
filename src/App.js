d554ff04ea0e5a10591ead27bfb8fd415a27be65
pankaj marghade <p.g.marghade@gmail.com>
2020-05-28 20:22:56 +0530

    working routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/not-found";
+import { Route, Redirect, Switch } from "react-router-dom";
+import NavBar from "./components/navbar";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <div className="m-4">
+          <main className="container">
+            <Switch>
+              <Route path="/movies" component={Movies}></Route>
+              <Route path="/customers" component={Customers}></Route>
+              <Route path="/rentals" component={Rentals}></Route>
+              <Route path="/not-found" component={NotFound}></Route>
+              <Route path="/" exact component={Movies} />
+              <Redirect to="/not-found"></Redirect>
+            </Switch>
+          </main>
+        </div>
+      </React.Fragment>
     );
   }
 }
